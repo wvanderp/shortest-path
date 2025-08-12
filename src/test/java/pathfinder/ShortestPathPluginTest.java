4fe0cbf3f48810603b9681537a7db69a2f19ef71
Wouter van der Plas <2423856+wvanderp@users.noreply.github.com>
2025-08-12 21:25:56 +0200

    Enhance testing and transport data: add JVM args for assertions and new boat routes

--- src/test/java/pathfinder/ShortestPathPluginTest.java
+++ src/test/java/pathfinder/ShortestPathPluginTest.java
@@ -1,12 +1,20 @@
 package pathfinder;
 
+import java.util.List;
+import java.util.ArrayList;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 import shortestpath.ShortestPathPlugin;
 
 public class ShortestPathPluginTest {
     public static void main(String[] args) throws Exception {
+
+        List<String> argList = new ArrayList<>();
+        java.util.Collections.addAll(argList, args);
+        argList.add("--developer-mode");
+        String[] modifiedArgs = argList.toArray(new String[0]);
+
         ExternalPluginManager.loadBuiltin(ShortestPathPlugin.class);
-        RuneLite.main(args);
+        RuneLite.main(modifiedArgs);
     }
 }
