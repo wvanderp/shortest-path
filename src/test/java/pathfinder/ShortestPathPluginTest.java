8119cba3c6ce3749e08e6a43ca7d9ba2d4fdf057
Wouter van der Plas <2423856+wvanderp@users.noreply.github.com>
2025-08-14 20:50:43 +0200

    Add settings for Java configuration and update test execution arguments

--- src/test/java/pathfinder/ShortestPathPluginTest.java
+++ src/test/java/pathfinder/ShortestPathPluginTest.java
@@ -1,20 +1,12 @@
 package pathfinder;
 
-import java.util.List;
-import java.util.ArrayList;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 import shortestpath.ShortestPathPlugin;
 
 public class ShortestPathPluginTest {
     public static void main(String[] args) throws Exception {
-
-        List<String> argList = new ArrayList<>();
-        java.util.Collections.addAll(argList, args);
-        argList.add("--developer-mode");
-        String[] modifiedArgs = argList.toArray(new String[0]);
-
         ExternalPluginManager.loadBuiltin(ShortestPathPlugin.class);
-        RuneLite.main(modifiedArgs);
+        RuneLite.main(args);
     }
 }
